4a72f5b5f826266a683538068ea819fa7a66f967
Rokas <Rokasberr@gmail.com>
2026-05-04 20:29:41 +0300

    Update storefront and subscription plan copy

--- client/src/constants/storefront.js
+++ client/src/constants/storefront.js
@@ -1,3 +1,3 @@
 export const STORE_PURCHASES_PAUSED = true;
 export const STORE_PURCHASES_PAUSED_MESSAGE =
-  "Šiuo metu Collection ir Digital produktai laikinai neperkeliami į krepšelį, kol ruošiame kitą atidarymo etapą.";
+  "Kolekcijos pirkimai trumpam pristabdyti, kol ruošiame tylesnį ir tikslesnį atidarymą.";
