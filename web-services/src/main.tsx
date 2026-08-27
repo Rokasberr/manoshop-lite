60f833a139f858707c1608bc5569352599aef3c4
Rokasberr <rokasberr@gmail.com>
2026-08-27 22:31:34 +0200

    feat(web): mount cookie consent banner

--- web-services/src/main.tsx
+++ web-services/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import CookieConsentBanner from "./components/CookieConsentBanner";
 import "./styles/main.css";
 import "./styles/polish.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
+    <CookieConsentBanner />
   </StrictMode>
 );
