128e851dcfef0ef9a5d2f24cb32756c29b7f66c7
Rokasberr <rokasberr@gmail.com>
2026-08-28 13:47:18 +0200

    style(web-footer): load refreshed footer styles

--- web-services/src/main.tsx
+++ web-services/src/main.tsx
@@ -14,6 +14,7 @@ import "./styles/background.css";
 import "./styles/experience.css";
 import "./styles/proposal.css";
 import "./styles/mobile-menu-fix.css";
+import "./styles/footer-refresh.css";
 
 captureLeadAttribution();
 initializeAnalytics();
