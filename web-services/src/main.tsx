2b55e78e87e6c704610ec168f650e6baf5dfe6be
Rokasberr <rokasberr@gmail.com>
2026-08-28 11:34:13 +0200

    feat(web-proposals): route secure proposal links

--- web-services/src/main.tsx
+++ web-services/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import CookieConsentBanner from "./components/CookieConsentBanner";
+import ProposalPage from "./ProposalPage";
 import { initializeAnalytics } from "./lib/analytics";
 import { captureLeadAttribution } from "./lib/leadAttribution";
 import { installLeadTracking } from "./lib/leadTracking";
@@ -10,14 +11,17 @@ import "./styles/polish.css";
 import "./styles/cookies.css";
 import "./styles/background.css";
 import "./styles/experience.css";
+import "./styles/proposal.css";
 
 captureLeadAttribution();
 initializeAnalytics();
 installLeadTracking();
 
+const proposalMatch = window.location.pathname.match(/^\/pasiulymas\/([a-f0-9]{64})\/?$/i);
+
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
+    {proposalMatch ? <ProposalPage token={proposalMatch[1]} /> : <App />}
     <CookieConsentBanner />
   </StrictMode>
 );
